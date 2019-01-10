8ac7daddd9e98979012caea9e6b78f8eb218da8b
TrueJugernaut <soh.labor@gmail.com>
2019-01-11 01:19:05 +0200

    fist empty commit

---
+++ src/main/java/pattern/factory/BuilderPattern.java
@@ -0,0 +1,42 @@
+package pattern.factory;
+
+public class BuilderPattern implements Pattern{
+    private String firstName;
+    private String lastName;
+
+    private BuilderPattern() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public static Builder newBuilder() {
+        return new BuilderPattern().new Builder();
+    }
+
+    @Override
+    public String getPatternName() {
+        return "Builder";
+    }
+
+    public class Builder {
+        public Builder() {
+        }
+        public Builder setFirstName() {
+            BuilderPattern.this.firstName = firstName;
+            return this;
+        }
+        public Builder setLastName() {
+            BuilderPattern.this.lastName = lastName;
+            return this;
+        }
+        public BuilderPattern build() {
+            return BuilderPattern.this;
+        }
+    }
+}
